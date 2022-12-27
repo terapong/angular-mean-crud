1bdb8781d14cc7d7bcefb88e0b03ba9af23e996d
terapong <terapong.p@gmail.com>
2022-12-27 14:46:43 +0700

    toto new

---
+++ src/app/service/crud.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+export class Book {
+  _id!: String;
+  name!: String;
+  price!: String;
+  description!: String;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  constructor() { }
+}
